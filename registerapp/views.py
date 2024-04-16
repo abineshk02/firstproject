b4e68ca10e6f4c22038beba46a9fcc2a5a54c263
abineshk02 <abineshk213@gmail.com>
2024-04-17 02:53:08 +0530

    pushed

---
+++ registerapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from . forms import EmployeeForm
+
+# Create your views here.
+def employee_list(request):
+    return render(request,"registerapp/employee_list.html")
+
+def employee_form(request):
+    if request.method == "GET":
+      form= EmployeeForm()
+      return render(request,"registerapp/employee_form.html",{'form':form})
+    else:
+      form=EmployeeForm(request.POST) 
+      if form.is_valid():
+         form.save()
+      return redirect('list/')
+
+def employee_delete(request):
+    return
